a556dd358ac421cf8bb0082fcce7c1643517a9f5
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-06-21 11:39:54 +0100

    Test complete

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+const submitData = (name, email) => {
+
+    const handleMyData = (data) => {
+        const h2 = document.createElement('h2')
+        const body = document.querySelector('body')
+        h2.textContent = data
+        body.appendChild(h2)
+    }
+
+    const handleMyErrors = (error) => {
+        const p = document.createElement('p')
+        const body = document.querySelector('body')
+        p.textContent = error
+        body.appendChild(p)
+    }
+    
+    const userData = {
+        name: name,
+        email: email
+    }
+    const objectConfig = {
+        method: "POST",
+        headers: {"Content-Type": "application/json", "Accept": "application/json"},
+        body: JSON.stringify(userData)
+    }
+    return fetch('http://localhost:3000/users', objectConfig)
+    .then(response => response.json())
+    .then(receivedData => handleMyData(receivedData.id))
+    .catch(e => handleMyErrors(e.message))
+}
+
+document.addEventListener('DOMContentLoaded', () => submitData("davui", 'dave@gmail.com'))
